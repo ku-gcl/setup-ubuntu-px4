2cf17e80c347cbdedec83e8ec392977b21d78455
shio777 <shiotatsu1015@gmail.com>
2025-06-05 15:11:48 +0900

    update: desktop and raspberrypi

---
+++ raspberrypi-setup.sh
@@ -0,0 +1,9 @@
+# Utils
+sudo apt update
+sudo apt upgrade
+sudo apt install -y git
+# sudo apt-get install code       # vscodeをダウンロード
+sudo apt install -y net-tools
+sudo apt install -y openssh-server
+
+
