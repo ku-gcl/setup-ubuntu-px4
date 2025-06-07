4eb7fb5d822c85abe44f6175ca1fabb2a59b0f3c
shio777 <shiotatsu1015@gmail.com>
2025-06-07 10:10:02 +0900

    update: simulation code

---
+++ simulation-command.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run PX4 SITL
+cd ~/PX4-Autopilot
+make px4_sitl gz_x500
+#for ubuntu20 run -> make px4_sitl gazebo-classic
+
+# Run DDS
+MicroXRCEAgent udp4 -p 8888
+
+# Run ROS2
+cd ~/ws_ros2
+source install/local_setup.bash
+ros2 run px4_ros_com offboard_control
