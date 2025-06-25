ac33430e5537b485e8e7921a3e0080fae37a8e6c
SHIO777 <shiotatsu1015@gmail.com>
2025-06-25 09:29:38 +0900

    update: UART settings

--- 5-raspberrypi-setup.sh
+++ 5-raspberrypi-setup.sh
@@ -11,3 +11,7 @@ sudo apt install -y ros-$ROS_DISTRO-vrpn-mocap
 # serial
 sudo apt install -y raspi-config
 
+# Enable UART
+sudo bash -c 'echo -e "\n# Enable UART settings\nenable_uart=1\ndtoverlay=disable-bt\ndtparam=uart0" >> /boot/firmware/config.txt'
+
+sudo reboot
