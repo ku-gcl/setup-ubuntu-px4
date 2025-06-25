d3b7a872ab6ddec0d3ae24aba068c8f449c7c3e1
SHIO777 <shiotatsu1015@gmail.com>
2025-06-25 09:31:43 +0900

    update: permission to /dev/ttyUSB0

--- 5-raspberrypi-setup.sh
+++ 5-raspberrypi-setup.sh
@@ -11,7 +11,11 @@ sudo apt install -y ros-$ROS_DISTRO-vrpn-mocap
 # serial
 sudo apt install -y raspi-config
 
-# Enable UART
+# Add permission to /dev/ttyUSB0
+sudo usermod -a -G dialout $USER
+sudo chmod a+rw /dev/ttyUSB0
+
+# Enable UART for serial connection Pixhawk and Raspberry Pi
 sudo bash -c 'echo -e "\n# Enable UART settings\nenable_uart=1\ndtoverlay=disable-bt\ndtparam=uart0" >> /boot/firmware/config.txt'
 
 sudo reboot
